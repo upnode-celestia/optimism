da5ff4ee540111e96dc1df517a2836ec216232d4
Josh Bowen <jskybowen@gmail.com>
2022-06-30 18:40:08 +0200

    fix header maybe?

--- op-node/l1/types.go
+++ op-node/l1/types.go
@@ -109,6 +109,7 @@ func (header *rpcHeader) Info(trustCache bool) (*HeaderInfo, error) {
 		txHash:      header.header.TxHash,
 		receiptHash: header.header.ReceiptHash,
 	}
+	fmt.Printf("header:\n%#+v", header.header)
 	if !trustCache {
 		if computed := header.header.Hash(); computed != info.hash {
 			return nil, fmt.Errorf("failed to verify block hash: computed %s but RPC said %s", computed, info.hash)
