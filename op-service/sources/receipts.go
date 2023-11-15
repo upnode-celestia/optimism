5bee876cd8a11a8ad802aa302967cecdaa3aae88
upnodedev <upresearchEditor@gmail.com>
2023-11-15 11:25:53 +0700

    Disable receipt hash verification mechanism

--- op-service/sources/receipts.go
+++ op-service/sources/receipts.go
@@ -10,7 +10,6 @@ import (
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/rpc"
-	"github.com/ethereum/go-ethereum/trie"
 
 	"github.com/ethereum-optimism/optimism/op-service/eth"
 )
@@ -80,11 +79,11 @@ func validateReceipts(block eth.BlockID, receiptHash common.Hash, txHashes []com
 
 	// Sanity-check: external L1-RPC sources are notorious for not returning all receipts,
 	// or returning them out-of-order. Verify the receipts against the expected receipt-hash.
-	hasher := trie.NewStackTrie(nil)
-	computed := types.DeriveSha(types.Receipts(receipts), hasher)
-	if receiptHash != computed {
-		return fmt.Errorf("failed to fetch list of receipts: expected receipt root %s but computed %s from retrieved receipts", receiptHash, computed)
-	}
+	// hasher := trie.NewStackTrie(nil)
+	// computed := types.DeriveSha(types.Receipts(receipts), hasher)
+	// if receiptHash != computed {
+	// 	return fmt.Errorf("failed to fetch list of receipts: expected receipt root %s but computed %s from retrieved receipts", receiptHash, computed)
+	// }
 	return nil
 }
 
