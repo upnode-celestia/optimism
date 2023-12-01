a3b15bdeda40a82b4128c006f6484e7c68ed533d
upnodedev <upresearchEditor@gmail.com>
2023-12-01 13:25:35 +0700

    Apply patch

--- op-batcher/flags/flags.go
+++ op-batcher/flags/flags.go
@@ -38,7 +38,6 @@ var (
 		Usage:   "HTTP provider URL for Rollup node",
 		EnvVars: prefixEnvVars("ROLLUP_RPC"),
 	}
-	// Optional flags
 	SubSafetyMarginFlag = &cli.Uint64Flag{
 		Name: "sub-safety-margin",
 		Usage: "The batcher tx submission safety margin (in #L1-blocks) to subtract " +
